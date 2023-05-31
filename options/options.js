633aedf1fe8414375ca0a3a887f25fd2b21780d7
Crowdbotics <engineering@crowdbotics.com>
2023-05-31 13:37:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "elkabanaapp-dev-87550",
+  url: "https://elkabanaapp-dev-87550.botics.co",
+  api: "https://elkabanaapp-dev-87550.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
